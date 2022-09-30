a9a50adca78b01ec30c120cd9aec8e59ae8189f2
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-30 21:31:55 +0100

    feats: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-5xl ">
+        navbar
+      </h1>
+    </div>
+  )
+}
+
+export default App
